392a9ca3838c1df12965a213fbc112f2aad08bb3
Alina Vlasova <alina_vlasova_01@bk.ru>
2024-12-11 13:56:19 +0300

    add mocks, create interface IProduct

---
+++ mocks/products.ts
@@ -0,0 +1,67 @@
+import type {IProduct} from "~/models/product-models";
+
+export const products: IProduct[] = [
+    {
+        id: 1,
+        name: 'BXC',
+        description: 'Вытяжное устройство для механической системы вентиляции',
+        price_ru: [6848, 56584],
+        price_eur: [77.60, 643.86],
+        article_number: 'Артикул: G2H1065',
+        img: 'public/img/BXC.png'
+    },
+    {
+        id: 2,
+        name: 'G2H',
+        description: 'Многофункциональное вытяжное устройство для естественной и гибридной вентиляции',
+        price_ru: [6848, 56584],
+        price_eur: [77.60, 643.86],
+        article_number: 'Артикул: G2H1065',
+        img: 'public/img/G2H.png'
+    },
+    {
+        id: 3,
+        name: 'GHN',
+        description: 'Вытяжное устройство с датчиком присутствия',
+        price_ru: [6848, 56584],
+        price_eur: [77.60, 643.86],
+        article_number: 'Артикул: G2H1065',
+        img: 'public/img/GHN.png'
+    },
+    {
+        id: 4,
+        name: 'TDA',
+        description: 'Вытяжное устройство с датчиком присутствия',
+        price_ru: [6848, 56584],
+        price_eur: [77.60, 643.86],
+        article_number: 'Артикул: G2H1065',
+        img: 'public/img/TDA.png'
+    },
+    {
+        id: 5,
+        name: 'TDA',
+        description: 'Вытяжное устройство с датчиком присутствия',
+        price_ru: [6848, 56584],
+        price_eur: [77.60, 643.86],
+        article_number: 'Артикул: G2H1065',
+        img: 'public/img/TDA.png'
+    },
+    {
+        id: 6,
+        name: 'TDA',
+        description: 'Вытяжное устройство с датчиком присутствия',
+        price_ru: [6848, 56584],
+        price_eur: [77.60, 643.86],
+        article_number: 'Артикул: G2H1065',
+        img: 'public/img/TDA.png'
+    },
+    {
+        id: 7,
+        name: 'TDA',
+        description: 'Вытяжное устройство с датчиком присутствия',
+        price_ru: [6848, 56584],
+        price_eur: [77.60, 643.86],
+        article_number: 'Артикул: G2H1065',
+        img: 'public/img/TDA.png'
+    }
+]
---
+++ models/product-models.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    price_ru: [number,number];
+    img: string;
+    article_number: string;
+    description: string;
+    price_eur: [number,number];
+}
